49b02a3c0101157819c6fd42f1d88639f72a6c8e
yohanes_l <yohanes.lumentut@yahoo.com>
2020-07-29 16:07:50 +0700

    v.0.2.1

--- lib/active_record/connection_adapters/db2_query_adapter.rb
+++ lib/active_record/connection_adapters/db2_query_adapter.rb
@@ -153,51 +153,6 @@ module ActiveRecord
         return 0 if in_use?
         Concurrent.monotonic_time - @idle_since
       end
-
-      private
-        def type_map
-          @type_map ||= Type::TypeMap.new.tap do |mapping|
-            initialize_type_map(mapping)
-          end
-        end
-
-        def alias_type(map, new_type, old_type)
-          map.register_type(new_type) do |_, *args|
-            map.lookup(old_type, *args)
-          end
-        end
-
-        def initialize_type_map(map)
-          map.register_type "boolean",              Type::Boolean.new
-          map.register_type ODBC::SQL_CHAR,         Type::String.new
-          map.register_type ODBC::SQL_LONGVARCHAR,  Type::Text.new
-          map.register_type ODBC::SQL_TINYINT,      Type::Integer.new(limit: 4)
-          map.register_type ODBC::SQL_SMALLINT,     Type::Integer.new(limit: 8)
-          map.register_type ODBC::SQL_INTEGER,      Type::Integer.new(limit: 16)
-          map.register_type ODBC::SQL_BIGINT,       Type::BigInteger.new(limit: 32)
-          map.register_type ODBC::SQL_REAL,         Type::Float.new(limit: 24)
-          map.register_type ODBC::SQL_FLOAT,        Type::Float.new
-          map.register_type ODBC::SQL_DOUBLE,       Type::Float.new(limit: 53)
-          map.register_type ODBC::SQL_DECIMAL,      Type::Float.new
-          map.register_type ODBC::SQL_NUMERIC,      Type::Integer.new
-          map.register_type ODBC::SQL_BINARY,       Type::Binary.new
-          map.register_type ODBC::SQL_DATE,         Type::Date.new
-          map.register_type ODBC::SQL_DATETIME,     Type::DateTime.new
-          map.register_type ODBC::SQL_TIME,         Type::Time.new
-          map.register_type ODBC::SQL_TIMESTAMP,    Type::DateTime.new
-          map.register_type ODBC::SQL_GUID,         Type::String.new
-
-          alias_type map, ODBC::SQL_BIT,            "boolean"
-          alias_type map, ODBC::SQL_VARCHAR,        ODBC::SQL_CHAR
-          alias_type map, ODBC::SQL_WCHAR,          ODBC::SQL_CHAR
-          alias_type map, ODBC::SQL_WVARCHAR,       ODBC::SQL_CHAR
-          alias_type map, ODBC::SQL_WLONGVARCHAR,   ODBC::SQL_LONGVARCHAR
-          alias_type map, ODBC::SQL_VARBINARY,      ODBC::SQL_BINARY
-          alias_type map, ODBC::SQL_LONGVARBINARY,  ODBC::SQL_BINARY
-          alias_type map, ODBC::SQL_TYPE_DATE,      ODBC::SQL_DATE
-          alias_type map, ODBC::SQL_TYPE_TIME,      ODBC::SQL_TIME
-          alias_type map, ODBC::SQL_TYPE_TIMESTAMP, ODBC::SQL_TIMESTAMP
-        end
     end
   end
 end
