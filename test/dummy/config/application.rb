87184037080564c572fa179d7554d4642c14571a
yohanes_l <yohanes.lumentut@gmail.com>
2021-07-05 08:58:04 +0700

    v.0.3.0

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,6 +1,9 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+require "active_record/railtie"
+require "active_storage/engine"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
