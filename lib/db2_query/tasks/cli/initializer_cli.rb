8a53121b98126ff4e1eb4a95b0f4c2c37413ec89
Yohanes <yohanes.lumentut@gmail.com>
2021-07-22 23:27:37 +0700

    Refactor db2_query:init CLI

--- lib/db2_query/tasks/cli/initializer_cli.rb
+++ lib/db2_query/tasks/cli/initializer_cli.rb
@@ -5,7 +5,7 @@ require_relative "base_cli"
 class InitializerCLI < BaseCLI
   source_root File.expand_path("../templates", __FILE__)
 
-  def create_database_config
+  def create_initializer_file
     template "initializer.rb", File.join("config/initializers/db2query.rb")
   end
 end
